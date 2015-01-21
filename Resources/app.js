b18d3dc9fb315b8033a5ed31a64489a4eb5ceaf4
Rainer Schleevoigt <fuerst@Rainers-MBP.fritz.box>
2015-01-21 21:36:54 +0100

    menu as module

--- Resources/app.js
+++ Resources/app.js
@@ -1,12 +1,12 @@
 Ti.UI.setBackgroundColor('#000');
 require('vendor/versionsreminder')();
-var torch = require('ti.light');
+
 var win = Titanium.UI.createWindow({
 	fullscreen : true,
 	exitOnClose : true,
 	backgroundColor : 'black'
 });
-var abx = require('com.alcoapps.actionbarextras');
+
 var bg = Ti.UI.createImageView({
 	bottom : 0,
 	width : Ti.UI.FILL,
@@ -27,8 +27,8 @@ if (Ti.Android) {
 	screenwidth *= (160 / Ti.Platform.displayCaps.xdpi);
 }
 var pages = [];
-var data = require('pagedata');
-data.forEach(function(item) {
+win.data = require('pagedata');
+win.data.forEach(function(item) {
 	pages.push(require('page.widget')(item));
 });
 var FlipModule = require('de.manumaticx.androidflip');
@@ -59,48 +59,13 @@ var onUpdateFunc = function(_e) {
 	pages.forEach(function(page) {
 		page.children[1].left = 50 + _e.x;
 	});
-
 };
 for (var i = 1; i < 5; i++)
 	pages[i].image.addEventListener('click', require('page' + i));
+	
+win.addEventListener('open', require('menu.widget')); 
 win.addEventListener('open', function() {
-	require('expansionfiles.events')();
 	Ti.Accelerometer.addEventListener('update', onUpdateFunc);
-	abx.title = "L u m i d i u m";
-	abx.subtitle = "the element of light";
-	abx.titleFont = "Sigward.ttf";
-	abx.titleColor = "#ff9";
-	var activity = win.getActivity();
-	if (activity) {
-		activity.onCreateOptionsMenu = function(e) {
-			e.menu.clear();
-			e.menu.add({
-				title : 'vimeo',
-				icon : Ti.App.Android.R.drawable.ic_action_v,
-				showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS,
-			}).addEventListener("click", function(_e) {
-				require('vimeo.window')().open();
-			});
-			e.menu.add({
-				title : 'FLASH',
-				icon : Ti.App.Android.R.drawable.ic_action_flash,
-				showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS,
-			}).addEventListener("click", function(_e) {
-				torch.toggle();
-			});
-			data.forEach(function(item, i) {
-				e.menu.add({
-					title : item.title,
-					showAsAction : Ti.Android.SHOW_AS_ACTION_NEVER,
-					itemId : i
-				}).addEventListener("click", function(_e) {
-					flipcontainer.flipToView(_e.source.itemId);
-					console.log(_e.source.itemId);
-					activity.invalidateOptionsMenu();
-				});
-			});
-		};
-	}
 });
 win.addEventListener('close', function() {
 	Ti.Accelerometer.removeEventListener('update', onUpdateFunc);
---
+++ Resources/menu.widget.js
@@ -0,0 +1,40 @@
+var abx = require('com.alcoapps.actionbarextras');
+var torch = require('ti.light');
+
+module.exports = function(_e) {
+	var data = _e.source.data;
+	abx.title = "L u m i d i u m";
+	abx.subtitle = "the element of light";
+	abx.titleFont = "Sigward.ttf";
+	abx.titleColor = "#ff9";
+	var activity = _e.source.getActivity();
+	if (activity) {
+		activity.onCreateOptionsMenu = function(e) {
+			e.menu.clear();
+			e.menu.add({
+				title : 'vimeo',
+				icon : Ti.App.Android.R.drawable.ic_action_v,
+				showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS,
+			}).addEventListener("click", function(_e) {
+				require('vimeo.window')().open();
+			});
+			e.menu.add({
+				title : 'FLASH',
+				icon : Ti.App.Android.R.drawable.ic_action_flash,
+				showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS,
+			}).addEventListener("click", function(_e) {
+				torch.toggle();
+			});
+			data.forEach(function(item, i) {
+				e.menu.add({
+					title : item.title,
+					showAsAction : Ti.Android.SHOW_AS_ACTION_NEVER,
+					itemId : i
+				}).addEventListener("click", function(_e) {
+					flipcontainer.flipToView(_e.source.itemId);
+					activity.invalidateOptionsMenu();
+				});
+			});
+		};
+	}
+}; 
