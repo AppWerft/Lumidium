f823676026ecd2463b353974bb1655d27797c18b
Rainer Schleevoigt <fuerst@Rainers-MBP.fritz.box>
2015-01-31 19:44:35 +0100

          

--- Resources/morse.player.js
+++ Resources/morse.player.js
@@ -1,61 +1,74 @@
-const TIME = {DIT : 50, DAH : 200, SPACE : 100};
+const TIME = {
+    DIT : 50,
+    DAH : 200,
+    SPACE : 100
+};
 var Torch = require('ti.light');
 var morse = require('morse');
 
 function sayDit(onready) {
     Torch.turnOn();
-    setTimeout(function(){
+    setTimeout(function() {
         Torch.turnOff();
-        setTimeout(onready,TIME.SPACE);
-    },TIME.DIT);
-}
-function sayDah(onready) {
-    Torch.turnOn();
-    setTimeout(function(){
-        Torch.turnOff();
-        setTimeout(onready,TIME.SPACE);
-    },TIME.DAH);
-}
-function sayBreak(onready) {
-    setTimeout(onready,TIME.SPACE);
+        setTimeout(onready, TIME.SPACE);
+    }, TIME.DIT);
 }
 
-module.exports  = function(args) {
-    var message = '';
-    if (args.message) { 
-        message = args.message; 
+function sayDah(onready) {
+    Torch.turnOn();
+    setTimeout(function() {
+        Torch.turnOff();
+        setTimeout(onready, TIME.SPACE);
+    }, TIME.DAH);
+}
+
+function sayBreak(onready) {
+    setTimeout(onready, TIME.SPACE);
+}
+
+/*
+ Parameters:
+ message : text, will automaticly coded to morse code
+ onready : optional ready callback
+ */
+
+module.exports = function() {
+    var args = arguments[0] || {};
+    var message = "";
+    if (args.message) {
+        message = args.message;
     } else {
         message = "morsecode";
     }
-   
-    function sayNext() {
-        var item = items.shift();  
-        if (item) {
-             sayItem(item);
-        } 
-    }
-    function sayItem(item) {
-        if (item == undefined) return;
-        switch (item) {
-            case '.':
-                sayDit(sayNext);
-            break;
-            case '-':
-                sayDah(sayNext);
-            break;
-            case ' ' :
-                sayBreak(sayNext);
-            break;
-            default:
-                args.onready && args.onready();
-            break;
-        }
-    }  
-    var morsecode = '';      
-    for (var i=0; i<message.length; i++) {
+    var morsecode = "";
+    for (var i = 0; i < message.length; i++) {
         morsecode += morse[message[i].toLowerCase()];
     }
-    var items = morsecode.split('');
-    
+    function sayNext() {
+        var item = items.shift();
+        if (item) {
+            sayItem(item);
+        }
+    }
+    function sayItem(item) {
+        if (item == undefined)
+            return;
+        switch (item) {
+        case '.':
+            sayDit(sayNext);
+            break;
+        case '-':
+            sayDah(sayNext);
+            break;
+        case ' ' :
+            sayBreak(sayNext);
+            break;
+        default:
+            args.onready && args.onready();
+            break;
+        }
+    }
+
+    var items = morsecode.split("");
     sayNext();
 };
