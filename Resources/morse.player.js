56cf964225fd33111168b1d25a1803391debfbd9
Rainer Schleevoigt <fuerst@Rainers-MBP.fritz.box>
2015-01-31 16:37:43 +0100

         

--- Resources/morse.player.js
+++ Resources/morse.player.js
@@ -1,21 +1,21 @@
-const time = {DIT : 100, DAH : 500, SPACE : 100};
+const TIME = {DIT : 100, DAH : 500, SPACE : 100};
 var Torch = require('ti.light');
 function sayDit(onready) {
     Torch.turnOn();
     setTimeout(function(){
         Torch.turnOff();
-        setTimeout(onready,time.SPACE);
-    },time.DIT);
+        setTimeout(onready,TIME.SPACE);
+    },TIME.DIT);
 }
 function sayDah(onready) {
     Torch.turnOn();
     setTimeout(function(){
         Torch.turnOff();
-        setTimeout(onready,time.SPACE);
-    },time.DAH);
+        setTimeout(onready,TIME.SPACE);
+    },TIME.DAH);
 }
 function sayBreak(onready) {
-    setTimeout(onready,time.SPACE);
+    setTimeout(onready,TIME.SPACE);
 }
 
 module.exports  = function(signal) {
